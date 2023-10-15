d4a22d64d50a42f2c5cef7d6fd1ba1ec006a041b
erceeren <ercee23@gmail.com>
2023-10-15 23:15:04 +0100

    Test create instance

--- main.tf
+++ main.tf
@@ -13,3 +13,39 @@ provider "google" {
 }
 
 
+resource "google_service_account" "default" {
+  account_id   = "my-custom-sa"
+  display_name = "Custom SA for VM Instance"
+}
+
+resource "google_compute_instance" "default" {
+  name         = "test-instance"
+  machine_type = "n2-standard-2"
+  zone         = "europe-west2-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  // Local SSD disk
+  scratch_disk {
+    interface = "NVME"
+  }
+
+  network_interface {
+    network = "default"
+
+    access_config {
+      // Ephemeral public IP
+    }
+  }
+
+  service_account {
+    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+    email  = google_service_account.default.email
+    scopes = ["cloud-platform"]
+  }
+}
+
